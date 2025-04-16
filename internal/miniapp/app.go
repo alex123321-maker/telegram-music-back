952075e04a8a070d6d9c59826d77cf2232b40b4f
alexander <alexander12110@gmail.com>
2025-04-17 04:43:41 +0700

    insta update to v3:)

--- internal/miniapp/app.go
+++ internal/miniapp/app.go
@@ -7,17 +7,18 @@ import (
 	"telegram-music/internal/miniapp/handler"
 	"telegram-music/pkg/logging"
 
-	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
 func Run(cfg *config.Config, logger logging.Logger) error {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "https://localhost:5173, https://mandrikov-ad.ru", // Разрешённые фронтенды
-		AllowMethods: "GET,POST,OPTIONS",
-		AllowHeaders: "Origin, Content-Type, Accept",
+		AllowOrigins:     []string{"https://localhost:5173, https://mandrikov-ad.ru"}, // Разрешённые фронтенды
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		AllowCredentials: true,
 	}))
 	registerRoutes(app)
 
