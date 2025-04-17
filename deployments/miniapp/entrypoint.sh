e324155cd9a510d685f81971c113ca9e6401aa68
alexander <alexander12110@gmail.com>
2025-04-17 16:44:17 +0700

    update Dockerfile and entrypoint script to use yt-dlp from GitHub releases and improve user creation

--- deployments/miniapp/entrypoint.sh
+++ deployments/miniapp/entrypoint.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 set -e
 
-# 1) скачиваем самую свежую сборку yt-dlp из GitHub Releases
-curl -L \
-  https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp \
-  -o ./yt-dlp \
-  && chmod a+rx ./yt-dlp
-
-# 2) применяем миграции
+# 1) применяем миграции
 migrate -path ./migrations -database "$DATABASE_URL" up
 
-# 3) запускаем сервер
+# 2) запускаем сервер
 exec ./server
