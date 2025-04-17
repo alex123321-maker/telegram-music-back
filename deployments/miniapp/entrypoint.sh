f43f663155a3acdbd88c535f7174989dc7e52e63
alexander <alexander12110@gmail.com>
2025-04-17 16:31:11 +0700

    ))

--- deployments/miniapp/entrypoint.sh
+++ deployments/miniapp/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 # 1) скачиваем самую свежую сборку yt-dlp из GitHub Releases
 curl -L \
   https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp \
-  -o /usr/local/bin/yt-dlp \
-  && chmod a+rx /usr/local/bin/yt-dlp
+  -o ./yt-dlp \
+  && chmod a+rx ./yt-dlp
 
 # 2) применяем миграции
 migrate -path ./migrations -database "$DATABASE_URL" up
