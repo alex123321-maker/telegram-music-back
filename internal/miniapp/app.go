46f4b004ccd2eb63ca843be5aa0c269ee6f1d4b4
alexander <alexander12110@gmail.com>
2025-05-18 02:08:35 +0700

    feat: add endpoint to retrieve user tags

--- internal/miniapp/app.go
+++ internal/miniapp/app.go
@@ -57,5 +57,6 @@ func registerRoutes(app *fiber.App) {
 	app.Post("/api/media", handler.GetMediaByTagsHandler)
 	app.Post("/api/tags", handler.CreateTagHandler)
 	app.Get("/api/tags", handler.ListTagsHandler)
+	app.Get("/api/my-tags", handler.GetUserTagsHandler)
 	app.Delete("/api/tags/:id", handler.DeleteTagHandler)
 }
